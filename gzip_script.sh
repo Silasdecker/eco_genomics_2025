951c3f25b9ed89450cd514a850017b23ff446548
Silas Decker <smdecker@hc-node01.cluster>
2025-11-11 17:26:29 -0500

    11_11_25_fastq_upload

---
+++ gzip_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=gz_fastq
+
+# Name the output file: Re-direct the log file to your home directory
+# The first part of the name (%x) will be whatever you name your job 
+#SBATCH --output=/gpfs1/cl/ecogen/pbio6800/GroupProjects/SpicyTomates/RawData
+
+# Which partition to use: options include short (<3 hrs), general (<48 hrs), or week
+#SBATCH --partition=general
+
+# Specify when Slurm should send you e-mail.  You may choose from
+# BEGIN, END, FAIL to receive mail, or NONE to skip mail entirely.
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=smdecker@uvm.edu
+
+# Run on a single node with four cpus/cores and 8 GB memory
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=64G
+
+# Time limit is expressed as days-hrs:min:sec; this is for 24 hours.
+#SBATCH --time=24:00:00
+
+#---------  End Slurm preamble, job commands now follow
+
+gzip SRR4102063_1.fastq
+gzip SRR4102063_2.fastq
+gzip SRR4102065_1.fastq
+gzip SRR4102065_2.fastq
+
